4ef7df2e30ac60e5a6e9b91a25493e6ae26d0d4f
wenhaowuuu <wenhaowu92@gmail.com>
2018-12-06 01:38:21 -0800

    1

---
+++ archive/js/main.js
@@ -0,0 +1,14 @@
+/* =====================
+   You should NOT need to change this file (though you are not forbidden from doing so)
+===================== */
+
+/* =====================
+  Call getAndParseData to grab our dataset through a jQuery.ajax call ($.ajax)
+===================== */
+//prepare the transformed data from csv
+
+
+//build the slider
+$(window).load(function() {
+  $("#container").twentytwenty();
+});
